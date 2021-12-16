0ddec25628ddf8e732fec8f01444d8a81cf2d9d1
Jacob Cambell <jacob@jacobcambell.com>
2021-12-16 11:24:42 -0700

    Add mobile design for LocationSelect

--- components/LocationSelect/LocationSelect.tsx
+++ components/LocationSelect/LocationSelect.tsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../fontClasses.module.scss";
 
 export default function LocationSelect() {
+  const [locations, setLocations] = useState<string[]>([
+    "All",
+    "Africa",
+    "Asia",
+    "USA",
+    "Canada",
+    "Latin America",
+    "Europe",
+  ]);
+
   return (
-    <div className={`${styles.fancy} bg-primarybg px-5 py-10`}>
+    <div
+      className={`${styles.fancy} bg-primarybg px-5 py-10 text-primarylight`}
+    >
       <select
         name=""
         id=""
-        className="bg-primarybg border border-primarylight text-primarylight text-xl m-auto block w-full p-3"
+        className="bg-primarybg border border-primarylight text-primarylight text-xl m-auto block w-full p-3 md:hidden"
       >
-        <option value="">All</option>
-        <option value="">Africa</option>
-        <option value="">Asia</option>
-        <option value="">Middle East</option>
+        {locations.map((loc) => {
+          return <option value="">{loc}</option>;
+        })}
       </select>
+
+      <div className="hidden md:flex items-center justify-center">
+        {locations.map((loc) => {
+          return (
+            <div>
+              <a className="text-xl mx-3 hover:underline" href="/">
+                {loc}
+              </a>
+              //
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
