324a25d7f178f40436e9e948599d87e2d985f28d
Jacob Cambell <jacob@jacobcambell.com>
2021-12-15 19:05:56 -0700

    Add custom Tailwind colors and Logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primarybg: "#242527",
+        primarylight: "#E4F5FF",
+      },
+    },
   },
   plugins: [],
 };
