e7f9c6289e973a4d7407b93179877e6893d36759
Jacob Cambell <jacob@jacobcambell.com>
2021-12-16 09:30:48 -0700

    Responsive design for Jumbo

--- components/Jumbo/Jumbo.tsx
+++ components/Jumbo/Jumbo.tsx
@@ -3,18 +3,26 @@ import styles from "../fontClasses.module.scss";
 
 export default function Jumbo() {
   return (
-    <div className="bg-primarybg py-5 px-5">
-      <img src="/bannerlady.png" alt="" className="m-auto" />
+    <div className="bg-primarybg py-5 overflow-auto">
+      <div className="w-4/5 m-auto overflow-auto lg:flex lg:items-center lg:flex-row-reverse 2xl:w-2/3">
+        <img
+          src="/bannerlady.png"
+          alt=""
+          className="m-auto w-2/3 md:w-1/2 lg:w-1/2 xl:w-1/3"
+        />
 
-      <div className={`text-center text-primarylight lg:text-red-500`}>
-        <p className={`text-3xl py-3 ${styles.fancy}`}>
-          Welcome to Women World!
-        </p>
-        <p>Enjoy browsing the site by region using the country below.</p>
-        <p>
-          For access to the complete database, which includes contact details,
-          languages spoken, info, and more...
-        </p>
+        <div className={`text-center text-primarylight lg:w-1/2 lg:text-left`}>
+          <p className={`text-3xl py-3 ${styles.fancy}`}>
+            Welcome to Women World!
+          </p>
+          <p className="text-lg pb-3">
+            Enjoy browsing the site by region using the country below.
+          </p>
+          <p className="text-lg">
+            For access to the complete database, which includes contact details,
+            languages spoken, info, and more...
+          </p>
+        </div>
       </div>
     </div>
   );
